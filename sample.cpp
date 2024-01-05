97d2065e1a68f9d65252dfd2e7e8c7b89d51a54d
balasainath <balasainath17@gmail.com>
2024-01-05 15:47:35 +0530

    second commit

---
+++ sample.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+void bubblesort();
+
+int insertionsort();
+
+int quicksort();
+
+int mergesort();
+
+int main()
+{
+cout<<"hii Balasai"<<endl;
+return 0;
+}
